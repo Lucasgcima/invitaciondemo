27f3f62f11c655e19fe5b36447c0b4921d733ea3
Lucas Cimaglia <lucascimaglia@gmail.com>
2025-07-11 22:18:03 -0300

    Update script.js

--- script.js
+++ script.js
@@ -22,10 +22,10 @@ function updateCountdown() {
 setInterval(updateCountdown, 1000);
 updateCountdown();
 
- window.addEventListener("DOMContentLoaded", () => {
-  const audio = document.getElementById("bg-music");
-  audio.muted = false;
-  audio.volume = 1;
+ const playBtn = document.getElementById("play-music");
+const audio = document.getElementById("bg-music");
+playBtn.addEventListener("click", () => {
   audio.play();
+  playBtn.style.display = "none";
 });
 
